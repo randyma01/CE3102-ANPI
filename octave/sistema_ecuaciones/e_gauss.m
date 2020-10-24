9972bdfb747706356acb313258d1afa23a52289c
Randy Martínez <randyma01@gmail.com>
2020-10-24 11:13:14 -0600

    sustituciones atras y adelantes: completadas

--- octave/sistema_ecuaciones/e_gauss.m
+++ octave/sistema_ecuaciones/e_gauss.m
@@ -2,25 +2,25 @@ function x = e_gauss(A,b)
 
     dim = size(A);
 
-    if (dim(1) == dim (2))
-        n = length(b); 
-        x = zeros(n, 1);
+    if (dim(1) != dim (2))
+        display("La matriz no es cuadrada")
+        return;
+    end
 
-        mat_aug = [A b]
+    n = length(b); 
+    x = zeros(n, 1);
 
-        for (j = 1 : n - 1)
-            for (i = j+1 : n)  
-                if (A(i, j) != 0)  
-                    lambda = A(i, j) / A(j, j);  
-                    A(i, j + 1 : n) = A(i, j + 1 : n) - (lambda * A(j, j + 1 : n));  
-                    b(i)= b(i) - (lambda * b(j));  
-            end  
-        end
-        %matA = mat_aug( : , 1 : end - 1)
-        %vecB = mat_aug(:, end)
-        x = sust_atras(A, b);  
-    %else
-        %disp('No es una matriz cuadrada.');
+    mat_aug = [A b];
+
+    for (j = 1 : n - 1)
+        for (i = j+1 : n)  
+            if (A(i, j) != 0)  
+                lambda = A(i, j) / A(j, j);  
+                A(i, j + 1 : n) = A(i, j + 1 : n) - (lambda * A(j, j + 1 : n));  
+                b(i)= b(i) - (lambda * b(j));  
+        end  
+    end
+    x = sust_atras(A, b);  
     end
 end
 % A = [2, -6, 12, 16; 1, -2, 6, 6; -1, 3, -3, -7; 0, 4, 3, -6]
