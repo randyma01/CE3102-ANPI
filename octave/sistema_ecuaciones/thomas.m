8ed17179d85b235e2ab726152fa632ce91466985
Randy Martínez <randyma01@gmail.com>
2020-10-24 23:46:30 -0600

    thomas no sirve

--- octave/sistema_ecuaciones/thomas.m
+++ octave/sistema_ecuaciones/thomas.m
@@ -1,4 +1,5 @@
-function [x] = thomas(A, b)  
+function [x] = thomas(A, d)
+    [m, n] = size(A);
 
     if (m != n)
         display("La matriz no es cuadrada.")
@@ -12,38 +13,60 @@ function [x] = thomas(A, b)
         return;
     end
 
-    if (vf_det_sub(A) != 1)
-        display("No cumple la condicion de determinantes de las submatrices.")
-        x = 0;
+     if (isbanded(A,1,1) == 0)
+        display("La matriz no es tridiagonal.")
+         x = 0;
         return;
     end
- 
-    n = size(a, 1);   %Recupera ordem do sistema.  
-    x = zeros(n, 1);  %Inicializa x  
+
+    A
+
+    a = [0];
+    a = [a diag(A, -1)'];
+
+    b = [diag(A)'];
+
+    c = [diag(A, 1)'];
+    c = [c 0];
+
+    d = d';
+
+    x = zeros(n, 1);  
     
-    c(1)=c(1)/b(1);  
-    for i=2:n-1  
-        c(i)=c(i)/(b(i)-a(i)*c(i-1));  
+    c(1) = c(1) / b(1);  
+
+    for (i = 2 : n - 1)  
+        (b(i) - a(i) * c(i - 1))  
+        c(i) = c(i) / (b(i) - a(i) * c(i - 1))  
     end  
-    
-    d(1)=d(1)/b(1);  
-    for i=2:n  
-        d(i)=(d(i)-a(i)*d(i-1))/(b(i)-a(i)*c(i-1));  
+
+    d(1) = d(1) / b(1);
+
+    for (i = 2 : n)  
+        d(i) = (d(i) - a(i) * d(i - 1)) / (b(i) - a(i) * c(i - 1));  
     end  
-    
-    x(n)=d(n);  
-    for i=n-1:-1:1  
-        x(i)=d(i)-c(i)*x(i+1);  
+
+    x(n) = d(n); 
+
+    for (i = n - 1: -1 : 1) 
+        x(i) = d(i) - c(i) * x(i + 1);  
     end  
 end
 
 % A = [1, 2, 0, 0; -1, 4, 3, 0; 0, 2, 1, 1; 0, 0, 1, 2]
-% b = [1; 1; 1; 1]
+% d = [1; 1; 1; 1]
 
-    % a = [0, -1, 2, 1]
-    % b = [1, 4, 1, 2]
-    % c = [2, 3, 1, 0]
+    % a = [0; -1; 2; 1]
+    % b = [1; 4; 1; 2]
+    % c = [2; 3; 1; 0]
+    % d = [1; 1; 1; 1]
+
+    % a = [0, 1, 2, 1, 2, 1]  
+    % b = [5, 3, 4, 2, 3, 2]  
+    % c = [4, 1, 1, 1, 2, 0]
+    % d = [13, 10, 20, 16, 35, 17]
+
 
 % x = thomas(a, b, c, d)
 
-% x = thomas(A, b)
+% x = thomas(A, d)
