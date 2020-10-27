4dcefea0fa61b0cdc82de15799dce83560f25ae2
Randy Martínez <randyma01@gmail.com>
2020-10-27 11:07:50 -0600

    agregando jacobi en c++

---
+++ c++/catalogo.jacobi/catalogo.jacobi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  catalogo.jacobi
+//
+//  Created by Randy Martinez on 10/27/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
