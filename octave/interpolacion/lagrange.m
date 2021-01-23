216990288adcdef42280b7e32dbe795565a63535
Randy Martínez <randyma01@gmail.com>
2021-01-23 14:02:02 -0600

    interpolacion: lagrange completado

---
+++ octave/interpolacion/lagrange.m
@@ -0,0 +1,23 @@
+function pn = lagrange(vx,vy)
+    syms x;
+    vx_len = length(vx);
+    vy_len = length(vy);
+    n = length(vx) - 1;
+    pn = 0;
+    
+    if (vx_len !=  vy_len)
+        print ("Los dos vectores deben contener el mismo numero de elementos.");
+    end
+    
+    for (k = 0 : n)
+        pn = pn + vy(k + 1) * lagrange_aux(vx, k);
+    end
+
+    pn = simplify(pn);
+    % ezplot(pn);
+
+end
+
+% px = [-2, 0, 1]
+% py = [0, 1, -1]
+% pn = lagrange (px, py)
