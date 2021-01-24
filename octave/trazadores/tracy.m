b9812ec2b9c0c8ebb3f3bfa74e561302d406a5ed
Randy Martínez <randyma01@gmail.com>
2021-01-24 00:12:25 -0600

    trazadores cubicos: 50%

--- octave/trazadores/tracy.m
+++ octave/trazadores/tracy.m
@@ -10,21 +10,26 @@ vy_len = length(vy);
 
 % Paso 1: Calcular hi %
 hi = zeros(1, (vx_len - 1));
+
 hi = vx(2 : vx_len) - vx(1 : (vx_len - 1));
+
 hi
 
 % Paso 2: Calcular ui %
 ui = zeros(1, vy_len - 2);
+
 for (i = 1 : (vy_len - 2))
     frac_1 = (vy(i+2) - vy(i+1)) / hi(i+1);
     frac_2 = (vy(i+1) - vy(i)) / hi(i);
     ui_t = 6 * (frac_1 - frac_2);
     ui(i) = [ui_t];
 end
+
 ui
 
 % Paso 3: Construir Matriz A %
 dim_A = vx_len - 2;
+
 A = zeros(dim_A, dim_A);
 for(i = 1 : dim_A)
     for (j = 1 : dim_A)
@@ -47,4 +52,5 @@ for (i = 1 : length(diag_inf))
 end 
 
 mat_A = diag(diag_sup, 1) + diag(diag(A)) + diag(diag_inf, -1);
+
 mat_A
